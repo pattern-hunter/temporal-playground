cce995b2d148b13d530b64a47e8c6b926d10ca1b
Tejas Sathe <sathe.tejas@gmail.com>
2024-11-15 17:42:35 -0500

    build example workflow

---
+++ Makefile
@@ -0,0 +1,2 @@
+run:
+	temporal server start-dev
---
+++ workflows/exampleWorkflow.go
@@ -0,0 +1,29 @@
+package workflows
+
+import (
+	"time"
+
+	"github.com/pattern-hunter/temporal-playground/activities"
+	"go.temporal.io/sdk/workflow"
+)
+
+func ExampleWorkflow(ctx workflow.Context, name string) (string, error) {
+	ao := workflow.ActivityOptions{
+		StartToCloseTimeout: 10 * time.Second,
+	}
+	ctx = workflow.WithActivityOptions(ctx, ao)
+
+	logger := workflow.GetLogger(ctx)
+	logger.Info("HelloWorld workflow started", "name", name)
+
+	var result string
+	err := workflow.ExecuteActivity(ctx, activities.ExampleActivity, name).Get(ctx, &result)
+	if err != nil {
+		logger.Error("Activity failed.", "Error", err)
+		return "", err
+	}
+
+	logger.Info("HelloWorld workflow completed.", "result", result)
+
+	return result, nil
+}
